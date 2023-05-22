760a3fc020633cc4a2103cf72b0814b22e5e8944
Saumya Tripathi <saumyatripathi15umesh@gmail.com>
2023-05-22 22:30:32 +0530

    first commit'

---
+++ js-learning/using-prompt_confirm/prac.js
@@ -0,0 +1,22 @@
+//write a code to approve for driving acc to your age 
+let age =prompt ("Enter your age");
+age = Number.parseInt(age);
+
+const candrive = (age)=>{
+    return age >= 18? true : false ;
+}
+// return user_conrm ;
+
+while(user_confrm){
+if(candrive(age)){
+    alert ("Yesh you can drive");
+}
+else{
+    alert ("you will not");
+}
+//*_  _  _  _    _    _   _  _   _   _   _  _  _  _  _    _    _   _  _   _   _   _  _  _  _  _    _    _   _  _   _   _   _*
+// Confir keyword useage
+
+user_a = confirm("Wanna try again");
+return user_a ();
+}
